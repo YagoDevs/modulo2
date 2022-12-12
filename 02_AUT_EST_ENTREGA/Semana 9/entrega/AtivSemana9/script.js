f09e99fb895031357b647d66f21e2d012b9b66fb
YagoDevs <110265917+YagoDevs@users.noreply.github.com>
2022-12-12 08:28:21 -0300

    colocando resto da ativ 9
    colocando resto da ativ 9

--- 02_AUT_EST_ENTREGA/Semana 9/entrega/AtivSemana9/script.js
+++ 02_AUT_EST_ENTREGA/Semana 9/entrega/AtivSemana9/script.js
@@ -1,27 +1,24 @@
-function vpvf(){
-    if(document.querySelector('input[name="valorfuturo"]:checked').value){
-        let valorpresente = parseFloat(document.getElementById("vf").value);
-        let i = parseFloat(document.getElementById("i").value)/100;
-        let n = parseFloat(document.getElementById("n").value);
-        let valorfuturo = valorpresente*(1+(i*n));
-        console.log(valorpresente)
-        console.log(i)
-        console.log(n)
-        console.log(valorfuturo)
-        
-        document.getElementById("resultado").innerHTML= valorfuturo
-    }
-    else if(document.querySelector('input[name="valorpresente"]:checked').value){
-        let valorfuturo = parseFloat(document.getElementById("vp").value);
-        let i = parseFloat(document.getElementById("i").value)/100;
-        let n = parseFloat(document.getElementById("n").value);
-        let valorpresente = valorfuturo/((1+i)^n);
-        document.getElementById("resultado").innerHTML= valorpresente
-    }
-    else{
-        alert("selecione um dos campos valor presente ou valor futuro")
-    }
+function valorFuturo(){
+    presente = parseFloat(document.getElementById("presente").value);
+    duracao = parseFloat(document.getElementById("duracao").value);
+    taxa = parseFloat(document.getElementById("taxa").value);
+
+    resultado = presente * (1+ (taxa/100)) ** duracao;
+
+    document.getElementById("resultado").innerHTML = resultado;
 }
+
+function valorPresente(){
+    futuro = parseFloat(document.getElementById("futuro").value);
+    duracao2 = parseFloat(document.getElementById("duracao2").value);
+    taxa2 = parseFloat(document.getElementById("taxa2").value);
+
+    resultado2 = futuro / (1 + (taxa2/100)) ** duracao2;
+
+    document.getElementById("resultado2").innerHTML = resultado2;
+}
+
+
 
 function juros(){
     let capital = parseFloat(document.getElementById("capital").value);
